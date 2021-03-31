40b113029fa320ee9a07f4167dff7a8caca80672
jessinthecloud <code@jessinthe.cloud>
2021-03-31 19:37:09 -0400

    start project

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+// sets the current file to strict typing
+// https://www.php.net/manual/en/functions.arguments.php#functions.arguments.type-declaration.strict
+declare(strict_types = 1); 
+
+require __DIR__ . '/../src/Bootstrap.php';
