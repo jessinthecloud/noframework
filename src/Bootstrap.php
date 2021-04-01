5f248de647769679e087adec10032764417aca8b
jessinthecloud <code@jessinthe.cloud>
2021-03-31 20:07:20 -0400

    added HTTP handler

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,6 +2,10 @@
 declare(strict_types = 1);
 
 namespace Example;
+// https://symfony.com/doc/current/components/http_foundation.html#installation
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 // makes sure we use Composer
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -22,7 +26,33 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-throw new \Exception;
+/*
+Equivalent to 
+$request = new Request(
+    $_GET,
+    $_POST,
+    [],
+    $_COOKIE,
+    $_FILES,
+    $_SERVER
+);
+ */
+$request = Request::createFromGlobals();
 
+/*
+holds all the information that needs to be sent back to the client from a given request
+ */
+$response = new Response(
+    'Hello World', // response content
+    Response::HTTP_OK, // status code; Response::HTTP_NOT_FOUND, etc
+    ['content-type' => 'text/html'] // HTTP headers
+);
 
-echo 'Hello World!';
+// When setting the Content-Type of the Response, you can set the charset, but it is better to set it via the setCharset() method:
+// $response->setCharset('UTF-8');
+// by default, Symfony assumes that your Responses are encoded in UTF-8
+
+// optionally call the prepare() method to fix any incompatibility with the HTTP specification (e.g. a wrong Content-Type header)
+$response->prepare($request);
+// send response to client
+$response->send();
