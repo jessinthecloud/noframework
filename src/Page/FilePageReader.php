062466d9d8bd69a1282b771a285736b6e29672af
jessinthecloud <code@jessinthe.cloud>
2021-04-02 15:14:33 -0400

    add comment about file reader

--- src/Page/FilePageReader.php
+++ src/Page/FilePageReader.php
@@ -7,13 +7,14 @@ use InvalidArgumentException;
 class FilePageReader implements PageReader
 {
     private $pageFolder;
-    
+
     // folder as constructor arg makes the class flexible and if we decide to move files or write unit tests for the class, we can easily change the location with the constructor argument
     public function __construct(string $pageFolder)
     {
         $this->pageFolder = $pageFolder;
     }
 
+    // TODO: probable shouldn't do this... check against whitelist?
     public function readBySlug(string $slug) : string
     {
         $path = "$this->pageFolder/$slug.md";
